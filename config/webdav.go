e251764e7eb55723d6e6b36c7b3c26b85f929ea0
Toby Yan <me@tobyan.com>
2019-04-25 00:56:28 +0800

    add webdav config
    Signed-off-by: Toby Yan <me@tobyan.com>

--- config/webdav.go
+++ config/webdav.go
@@ -7,8 +7,20 @@ import (
 func init() {
 	webdav := make(map[string]interface{})
 
-	webdav["filesystem"] = "memory" // file, memory
+	webdav["driver"] = Env("WEBDAV_DRIVER", "memory") // file, memory, minio
 
+	webdav["filesystems"] = map[string]interface{}{
+		"file": map[string]interface{}{
+			"base_path": ".",
+		},
+		"minio": map[string]interface{}{
+			"endpoint": Env("MINIO_ENDPOINT", "play.min.io:9000"),
+			"access_key_id": Env("MINIO_ACCESS_KEY_ID", "access_key_id"),
+			"secret_access_key": Env("MINIO_SECRET_ACCESS_KEY", "secret_access_key"),
+			"use_ssl": Env("MINIO_USE_SSL", true),
+			"location": "us-east-1",
+		},
+	}
 	webdav["base_path"] = "." // for "file" filesystem
 	webdav["supported_folder_depth"] = 10
 	webdav["base_url"] = "/webdav"
