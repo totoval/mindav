4a1e91d16bb4b72189e91272be2749c1191833a1
Toby Yan <me@tobyan.com>
2019-04-25 18:56:40 +0800

    fix sub folder list issue
    Signed-off-by: Toby Yan <me@tobyan.com>

--- app/logics/webdav/driver/minio.go
+++ app/logics/webdav/driver/minio.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"github.com/minio/minio-go"
 	"github.com/totoval/framework/config"
-	"github.com/totoval/framework/helpers/debug"
 	"golang.org/x/net/webdav"
 	"log"
 	"os"
@@ -71,11 +70,15 @@ func (m Minio) MkBucket() (err error) {
 	log.Printf("Successfully created %s\n", m.bucketName)
 	return nil
 }
-
+func (m Minio) resolve() string {
+	return ""
+}
 func (m Minio)Mkdir(ctx context.Context, name string, perm os.FileMode) error {
-	const KEEP_FILE_NAME = ".keep"
+	const KEEP_FILE_NAME = ".mindavkeep"
 
-	name = strings.TrimPrefix(name, "/")
+	if name != "/"{
+		name = strings.TrimPrefix(name, "/")
+	}
 
 	fileBytes := bytes.NewBuffer([]byte{})
 	n, err := m.client.PutObject(m.bucketName, name + KEEP_FILE_NAME, bytes.NewBuffer([]byte{}), int64(fileBytes.Len()), minio.PutObjectOptions{ContentType: "application/octet-stream"})
@@ -87,17 +90,25 @@ func (m Minio)Mkdir(ctx context.Context, name string, perm os.FileMode) error {
 	return nil
 }
 func (m Minio)OpenFile(ctx context.Context, name string, flag int, perm os.FileMode) (webdav.File, error){
-	if strings.HasSuffix(name, "/") {
-		return MinioObject{&m, nil, name}, nil
+	log.Println(" --- Minio OpenFile "+name)
+
+	if name != "/"{
+		name = strings.TrimPrefix(name, "/")
 	}
+
+	if name == "/" {
+		return minioObject{&m, nil, name}, nil
+	}
+
 
 	// file
 	object, err := m.client.GetObject(m.bucketName, name, minio.GetObjectOptions{})
+	log.Println("OPEN---------------------------------"+name)
 	if err != nil {
 		return nil, err
 	}
 
-	return MinioObject{&m, object, name}, nil
+	return minioObject{&m, object, name}, nil
 }
 func(m Minio)RemoveAll(ctx context.Context, name  string) error {
 	return nil
@@ -106,106 +117,133 @@ func (m Minio)Rename(ctx context.Context, oldName, newName string) error {
 	return nil
 }
 
-type MinioObject struct {
+type minioObject struct {
 	m *Minio
 	*minio.Object
 	name string
 }
 
-func (mo MinioObject) Stat() (os.FileInfo, error) {
+func (mo minioObject) Stat() (os.FileInfo, error) {
+	log.Println(" --- minioObject Stat "+mo.name)
 
-	if strings.HasSuffix(mo.name, "/"){
-		return objectInfo{minio.ObjectInfo{
-				Key: mo.name,
-				Size: 0,
-				LastModified: time.Now(),
-				ContentType: "inode/directory",
-		}}, nil
+	_, err := mo.m.client.StatObject(mo.m.bucketName, mo.name, minio.StatObjectOptions{})
+	if err != nil {
+		log.Println("minioObject stat", err)
+		if _err, ok := err.(minio.ErrorResponse); ok {
+			if _err.Code == "NoSuchKey" {
+				// dir
+				return minioObjectInfo{minio.ObjectInfo{
+					Key: mo.name,
+					Size: 0,
+					LastModified: time.Now(),
+					ContentType: "inode/directory",
+				}}, nil
+			}
+		}
+		return nil, err
 	}
+	// if strings.HasSuffix(mo.name, "/"){
+	// 	return minioObjectInfo{minio.ObjectInfo{
+	// 			Key: mo.name,
+	// 			Size: 0,
+	// 			LastModified: time.Now(),
+	// 			ContentType: "inode/directory",
+	// 	}}, nil
+	// }
 	oi, err := mo.Object.Stat()
-	return objectInfo{oi}, err
+	return minioObjectInfo{oi}, err
 }
 
-func (mo MinioObject) Write(p []byte) (n int, err error) {
+func (mo minioObject) Write(p []byte) (n int, err error) {
 	panic("implement me")
 	return 0, nil //@todo
 }
 
-func(mo MinioObject)Readdir(count int)(fileInfoList []os.FileInfo, err error){
+func(mo minioObject)Readdir(count int)(fileInfoList []os.FileInfo, err error){
+	log.Println(" --- minioObject Readdir "+mo.name)
+	name := strings.TrimPrefix(mo.name, "/")
 	// Create a done channel to control 'ListObjects' go routine.
 	doneCh := make(chan struct{})
 
 	// Indicate to our routine to exit cleanly upon return.
 	defer close(doneCh)
 	// List all objects from a bucket-name with a matching prefix.
-	for object := range mo.m.client.ListObjects(mo.m.bucketName, mo.name, false, doneCh) {
+	for object := range mo.m.client.ListObjects(mo.m.bucketName, name, false, doneCh) {
 		err = object.Err
 		if err != nil {
 			fmt.Println(object.Err)
 			//return
 			break
 		}
-		fileInfoList = append(fileInfoList, objectInfo{object})
+		fileInfoList = append(fileInfoList, minioObjectInfo{object})
 	}
 	return fileInfoList, err
 }
 func (m Minio)Stat(ctx context.Context, name string) (os.FileInfo, error) {
-	if strings.HasSuffix(name, "/") {
-		// dir
-		return objectInfo{minio.ObjectInfo{
-			Key: name,
-			Size: 0,
-			LastModified: time.Now(),
-			ContentType: "inode/directory",
-		}}, nil
+	log.Println(" --- Minio Stat "+name)
+
+	if name != "/"{
+		name = strings.TrimPrefix(name, "/")
 	}
 
 	stat, err := m.client.StatObject(m.bucketName, name, minio.StatObjectOptions{})
-	debug.Dump(ctx, name, stat, err)
 	if err != nil {
 		log.Println(err)
+		if _err, ok := err.(minio.ErrorResponse); ok {
+			if _err.Code == "NoSuchKey" {
+				// dir
+				return minioObjectInfo{minio.ObjectInfo{
+					Key: name,
+					Size: 0,
+					LastModified: time.Now(),
+					ContentType: "inode/directory",
+				}}, nil
+			}
+		}
 		return nil, err
 	}
 	log.Println(stat)
-	return objectInfo{stat}, nil
+	return minioObjectInfo{stat}, nil
 }
-func (m Minio)ReadDir(name string){
-	// Create a done channel to control 'ListObjectsV2' go routine.
-	doneCh := make(chan struct{})
+// func (m Minio)ReadDir(name string){
+// 	// Create a done channel to control 'ListObjectsV2' go routine.
+// 	doneCh := make(chan struct{})
+//
+// 	// Indicate to our routine to exit cleanly upon return.
+// 	defer close(doneCh)
+//
+// 	isRecursive := false
+// 	objectCh := m.client.ListObjectsV2(m.bucketName, name, isRecursive, doneCh)
+// 	for object := range objectCh {
+// 		if object.Err != nil {
+// 			fmt.Println(object.Err)
+// 			return
+// 		}
+// 		fmt.Println(object)
+// 	}
+// }
 
-	// Indicate to our routine to exit cleanly upon return.
-	defer close(doneCh)
-
-	isRecursive := false
-	objectCh := m.client.ListObjectsV2(m.bucketName, name, isRecursive, doneCh)
-	for object := range objectCh {
-		if object.Err != nil {
-			fmt.Println(object.Err)
-			return
-		}
-		fmt.Println(object)
-	}
-}
-
-type objectInfo struct {
+type minioObjectInfo struct {
 	minio.ObjectInfo
 }
-func (oi objectInfo)Name() string {
-	return oi.ObjectInfo.Key
+func (moi minioObjectInfo)Name() string {
+	n := strings.TrimSuffix(moi.ObjectInfo.Key, "/")
+	t := strings.Split(n, "/")
+	return t[len(t)-1]//moi.ObjectInfo.Key
 }       // base name of the file
-func (oi objectInfo)Size() int64 {
-	return oi.ObjectInfo.Size
+func (moi minioObjectInfo)Size() int64 {
+	return moi.ObjectInfo.Size
 }       // length in bytes for regular files; system-dependent for others
-func (oi objectInfo)Mode() os.FileMode {
+func (moi minioObjectInfo)Mode() os.FileMode {
 	return 777
 }    // file mode bits
-func (oi objectInfo)ModTime() time.Time {
-	return oi.ObjectInfo.LastModified
+func (moi minioObjectInfo)ModTime() time.Time {
+	return moi.ObjectInfo.LastModified
 }// modification time
-func (oi objectInfo)IsDir() bool {
-	fmt.Println(oi.ObjectInfo.ContentType)
-	return oi.ObjectInfo.ContentType == "inode/directory"
+func (moi minioObjectInfo)IsDir() bool {
+	fmt.Println(moi.Name()+" isdir ", moi.ObjectInfo.ContentType == "inode/directory")
+	return moi.ObjectInfo.ContentType == "inode/directory"
 }       // abbreviation for Mode().IsDir()
-func (oi objectInfo)Sys() interface{} {
+func (moi minioObjectInfo)Sys() interface{} {
 	return nil
 }  // underlying data source (can return nil)
